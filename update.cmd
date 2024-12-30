78e0fc70707e88a0d437462c48a43470f7120909
resistor3300 <resistor330@gmail.com>
2024-12-30 19:47:17 +0700

    update

--- update.cmd
+++ update.cmd
@@ -1,7 +1,17 @@
 @echo off
+:: Di chuyển chính file update.cmd ra khỏi thư mục tạm
+set TEMP_CMD=%TEMP%\update-temp.cmd
+copy "%~f0" "%TEMP_CMD%" >nul
+
+:: Thực hiện các lệnh Git
 cd /d "%~dp0"
+git fetch
+git reset --hard HEAD
 git pull
-echo update ok men!
-npm install
-echo done men!
+
+:: Di chuyển file update.cmd quay lại vị trí cũ (nếu cần)
+copy "%TEMP_CMD%" "%~f0" >nul
+del "%TEMP_CMD%" >nul
+
+echo Update completed!
 pause
