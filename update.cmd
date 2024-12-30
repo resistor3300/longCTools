626da4ab4f2f184185ce94ac54eefd36cdf10a8f
resistor3300 <resistor330@gmail.com>
2024-12-30 19:52:13 +0700

    update

--- update.cmd
+++ update.cmd
@@ -1,17 +1,25 @@
 @echo off
-:: Di chuyển chính file update.cmd ra khỏi thư mục tạm
+:: Bước 1: Lưu đường dẫn file hiện tại vào một biến
+set CURRENT_CMD=%~f0
 set TEMP_CMD=%TEMP%\update-temp.cmd
-copy "%~f0" "%TEMP_CMD%" >nul
 
-:: Thực hiện các lệnh Git
+:: Bước 2: Sao chép chính file `update.cmd` vào vị trí tạm để tiếp tục chạy sau khi cập nhật
+copy "%CURRENT_CMD%" "%TEMP_CMD%" >nul
+
+:: Bước 3: Chuyển vào thư mục chứa Git repository
 cd /d "%~dp0"
+
+:: Bước 4: Thực hiện các lệnh Git
 git fetch
 git reset --hard HEAD
 git pull
 
-:: Di chuyển file update.cmd quay lại vị trí cũ (nếu cần)
-copy "%TEMP_CMD%" "%~f0" >nul
-del "%TEMP_CMD%" >nul
+:: Bước 5: Ghi đè file `update.cmd` từ Git nếu có thay đổi
+if exist "%TEMP_CMD%" (
+    echo Updating self...
+    copy /y "%~dp0update.cmd" "%CURRENT_CMD%" >nul
+    del "%TEMP_CMD%" >nul
+)
 
-echo Update completed! ok mèn đét ơi
+echo Update completed!
 pause
