f403b389cb671127b154f78d9729160a0070fe6a
resistor3300 <resistor330@gmail.com>
2025-04-13 19:24:14 +0700

    Up 4.1.9

--- Run.cmd
+++ Run.cmd
@@ -1,4 +1,14 @@
 @echo off
 cd /d "%~dp0"
-node task.js
-pause
+set TIMEOUT_SECONDS=120
+node --expose-gc task.js
+
+:loop
+echo Vui long cho, Chuong trinh se chay lai sau %TIMEOUT_SECONDS%s...
+timeout /t %TIMEOUT_SECONDS% /nobreak
+
+echo Bat dau chay...
+node --expose-gc task.js
+
+echo Chuong trinh da thoat, Tu dong chay lai sau %TIMEOUT_SECONDS%s...
+goto loop
